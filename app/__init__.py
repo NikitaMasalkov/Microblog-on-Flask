455b865e9af33671f72bde2d565a8322b6cadc07
nikitamasalkov <nikitamasalkov.tang@gmail.com>
2018-11-25 18:42:28 +0300

    added DB boilerplate

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 app.config.from_object(Config)
 app.debug = True
 FLASK_DEBUG = 1
-db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
