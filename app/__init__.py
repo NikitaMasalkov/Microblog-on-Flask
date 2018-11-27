866947779de9597725d2d81a01abfe2e6e0f6419
nikitamasalkov <nikitamasalkov.tang@gmail.com>
2018-11-27 21:08:11 +0300

    Check completion mark on Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 app.config.from_object(Config)
 app.debug = True
